f87d59ae284db9914050e43aa4d0513ec1ce5e17
Jean Ribeiro Garcia <jeanrgarcia@gmail.com>
2026-09-23 21:57:22 -0300

    chore: initial project from Spring Initializr

---
+++ src/test/java/dev/jeanrgarcia/timeline_api/TimelineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeanrgarcia.timeline_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
